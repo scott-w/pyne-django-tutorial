8f782c0a9777160fa21a987422664bdfb87c9891
Scott Walton <s@cott.me.uk>
2014-12-30 20:31:01 +0000

    Created our model and admin
    Created the base model class and registered with the admin.

--- chatter/chatter/base/models.py
+++ chatter/chatter/base/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class Chat(models.Model):
+    """A single chat from a User.
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_add_now=True)
